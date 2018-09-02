1ccb27f146b869c95a07285336a296d1da6a526d
Kristen Hallstrom <klhalls89@gmail.com>
2018-09-02 17:26:43 -0600

    Start JSON

--- script.js
+++ script.js
@@ -6,11 +6,54 @@ var cardSection = document.querySelector('.card-section');
 
 saveBtn.addEventListener('click', createCard);
 cardSection.addEventListener('click', deleteCard);
+titleInput.addEventListener('keyup', toggleSaveButton);
+bodyInput.addEventListener('keyup', toggleSaveButton);
+
+toggleSaveButton();
+displayCards();
+
+function toggleSaveButton(){
+  if(titleInput.value === '' || bodyInput.value === ''){
+    saveBtn.disabled = true;
+  } else {
+    saveBtn.disabled = false;
+  }
+}
+
+function displayCards(e) {
+  JSON.parse(localStorage.getItem("storedCardArray")).forEach(function(potato) {
+    cardHTML(potato);
+  });
+};
+
+function CardConstructor(title, body) {
+  this.id = Date.now();
+  this.title = title;
+  this.body = body;
+};
 
 function createCard(event) {
-event.preventDefault();
-cardSection.innerHTML += `
-<article class="card-article">
+  event.preventDefault();
+  var newCard = new CardConstructor(titleInput.value, bodyInput.value); 
+  cardHTML(newCard);
+  var cardArray = [];
+  if (JSON.parse(localStorage.getItem("storedCardArray"))) {
+    cardArray = JSON.parse(localStorage.getItem("storedCardArray")); 
+    cardArray.push(newCard);
+    localStorage.setItem('storedCardArray', JSON.stringify(cardArray));
+  } else {
+    cardArray.push(newCard);
+    localStorage.setItem('storedCardArray', JSON.stringify(cardArray));
+  }
+  titleInput.value = '';
+  bodyInput.value = '';
+  titleInput.focus();
+  toggleSaveButton();
+}
+
+function cardHTML(object) {
+  cardSection.innerHTML += `
+  <article class="card-article">
     <div class="card-top">
       <h3 class="card-title">${titleInput.value}</h3>
       <i class="far fa-times-circle delete-btn"></i>
@@ -23,14 +66,15 @@ cardSection.innerHTML += `
       <p class="selected-quality">swill</p>
     </div>
   </article>`
-titleInput.value = '';
-bodyInput.value = '';
-titleInput.focus();
 }
-
 
 function deleteCard(event){
   if (event.target.classList.contains('delete-btn')) {
     event.target.parentNode.parentNode.remove();
   }
 }
+
+
+// localStorage.setItem('storedCardArray', JSON.stringify(cardArray))
+
+
